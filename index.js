2ee49f9768463867f1c8904d763eebdea7e93ede
EvanCobb <evan.cobb@protonmail.com>
2020-11-09 16:22:06 -0600

    trees create (not comits dont')

--- index.js
+++ index.js
@@ -17,29 +17,31 @@ const main = async function() {
     const repo = "test-terraform"
     const owner = "icbat"
 
-    const {name, sha: parent_sha} = await get_default_branch({owner, repo})
+    const { name } = await get_default_branch({owner, repo})
     const ref = `refs/heads/${name}`
 
+    const {data: {object: {sha: latest_commit_sha} }} = await octokit.git.getRef({ owner, repo, ref: `heads/${name}`})
+
     const content = "thisIsContent"
-    const encoding = 'utf-8' // only other option is base64, we probably wnat that to minimize bandwidth usage, but node doesn't have it naturally
+    const encoding = 'utf-8' // only other option is base64, we probably wnat that to minimize bandwidth usage, will need to import
     const { data: {sha: blob_sha} } = await octokit.git.createBlob({owner, repo, content, encoding})
     console.log('blob sha', blob_sha)
     
-    const tree = {
-        path: 'asdf.txt',
+    const tree = [{
+        path: 'README.md',
         mode: '100644', // blob  (but not executable)
         type: 'blob',
         // sha: null, // use only if you do an individual step to make the blog, if so kill content
         // content: 'this is content definitely \n a great file',
         sha: blob_sha, // use only if you do an individual step to make the blog, if so kill content
         // content: 'this is content definitely \n a great file',
-    }
+    }]
 
-    const {data: {sha: tree_sha}} = await octokit.git.createTree({owner, repo, tree})
+    const { data: { sha: tree_sha } } = await octokit.git.createTree({ owner, repo, tree, base_tree: latest_commit_sha})
     console.log("tree created", tree_sha)
 
     const message = `Automated commit - SPIKE REPO: ${Math.random()}`
-    const parents = [parent_sha]
+    const parents = [latest_commit_sha]
     // author and committer are optional args here for overriding the auth'd user as the committer/author. could be useful.
     const {data: { sha: new_commit_sha }} = await octokit.git.createCommit({ owner, repo, message, tree_sha, parents})
 
@@ -47,5 +49,5 @@ const main = async function() {
     await octokit.git.updateRef({owner, repo, ref, sha: new_commit_sha})
 }
 
-main().catch(e => console.error(e.headers.status))
+main().catch(e => console.error(e, e.headers.status))
 
